bbee4bfc73bfc04b7d8ea24ec6f6e7f2e899878d
Beth Fostergit config --global user.namegit config --global user.name Beth <beth@techgir1.com>
2016-01-28 15:47:26 -0800

    first spec passed

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+function Customer(customerName, customerAddress, customerPhone) {
+	this.customerName = String(customerName);
+	this.customerAddress = String(customerAddress);
+	this.customerPhone = String(customerPhone);
+}
---
+++ spec/specs.js
@@ -0,0 +1,8 @@
+describe("Customer", function() {
+	it("creates a new customer", function() {
+		var testCustomer = new Customer("Beth", "1116 SE Division St", "503-111-1116");
+		expect(testCustomer.customerName).to.equal("Beth");
+		expect(testCustomer.customerAddress).to.equal("1116 SE Division St");
+		expect(testCustomer.customerPhone).to.equal("503-111-1116")
+	});
+});
