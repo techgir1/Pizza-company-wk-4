2157a74bafe69290b4f77e2acd191eff366fc382
Beth Fostergit config --global user.namegit config --global user.name Beth <beth@techgir1.com>
2016-01-28 15:53:16 -0800

    second spec passed

--- spec/specs.js
+++ spec/specs.js
@@ -5,4 +5,9 @@ describe("Customer", function() {
 		expect(testCustomer.customerAddress).to.equal("1116 SE Division St");
 		expect(testCustomer.customerPhone).to.equal("503-111-1116")
 	});
+
+	it("handles a phone number as a string", function() {
+		var testCustomer = new Customer("Beth", "1116 SE Division St", "503-111-1116");
+		expect(testCustomer.customerPhone).to.be.a("string");
+	});
 });
