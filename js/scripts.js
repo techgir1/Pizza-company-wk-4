5af2a2c75ef2f28405585db39d43528227862508
Beth Fostergit config --global user.namegit config --global user.name Beth <beth@techgir1.com>
2016-01-28 19:54:48 -0800

    added image. Pizza ordering logic working. Some initial styling

--- js/scripts.js
+++ js/scripts.js
@@ -5,31 +5,46 @@ function Customer(customerName, customerAddress, customerPhone) {
 }
 
 function Pizza(quantity, size) {
-	this.quantity = quantity;
-	this.size = size;
-	this.vegatableTopping = [];
-	this.meatTopping = [];
+  this.quantity = quantity;
+  this.size = size;
+  this.vegatableTopping = [];
+  this.meatTopping =[];
 };
 
-Pizza.prototype.Cost = function() {
-	var totalCost = 8 + (this.vegatableTopping.length * 1) + (this.meatTopping.length * 2);
+Pizza.prototype.cost = function() {
+  var totalCost = 8 + (this.vegatableTopping.length * 1) + (this.meatTopping.length * 2);
 
-	if (this.size == "small") {
-		return totalCost * this.quantity;
-	} else if (this.size == "medium") {
-		return (totalCost + 4) * this.quantity;
-	} else if (this.size == "large") {
-		return (totalCost + 10) * this.quantity;
-	} else if (this.size == "x-large") {
-		return (totalCost + 14) * this.quantity;
-	}
-};
+  if (this.size == "small") {
+    return totalCost * this.quantity;
+  } else if (this.size == "medium") {
+      return (totalCost + 4) * this.quantity;
+    } else if (this.size == "large") {
+        return (totalCost + 10) * this.quantity;
+    } else if (this.size == "x-large") {
+        return (totalCost + 14) * this.quantity;
+    }
+  };
 
+$(document).ready(function() {
 
+  $("form#order-form").submit(function(event) {
+    var quantity = $("input#quantity").val();
+    var size = $("select#size").val();
+    var newPizza = new Pizza(quantity, size);
+    var vegatableTopping = $("input.vegatable:checkbox:checked").each(function() {
+      newPizza.vegatableTopping.push(vegatableTopping);
+    });
 
+    var meatTopping = $("input.meat:checkbox:checked").each(function() {
+      newPizza.meatTopping.push(meatTopping);
+    });
 
+    var pizzaPrice = newPizza.cost();
 
+    $("#price").text(pizzaPrice);
 
+    $("#result").show();
 
-
-
+    event.preventDefault();
+  });
+});
