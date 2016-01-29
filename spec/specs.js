415ed6556ab80c34b758cdf5d71ba499ab8cffb5
Beth Fostergit config --global user.namegit config --global user.name Beth <beth@techgir1.com>
2016-01-29 13:56:46 -0800

    took out customer information from specs

--- spec/specs.js
+++ spec/specs.js
@@ -1,16 +1,3 @@
-// describe("Customer", function() {
-// 	it("creates a new customer", function() {
-// 		var testCustomer = new Customer("Beth", "1116 SE Division St", "503-111-1116");
-// 		expect(testCustomer.customerName).to.equal("Beth");
-// 		expect(testCustomer.customerAddress).to.equal("1116 SE Division St");
-// 		expect(testCustomer.customerPhone).to.equal("503-111-1116")
-// 	});
-
-// 	it("handles a phone number as a string", function() {
-// 		var testCustomer = new Customer("Beth", "1116 SE Division St", "503-111-1116");
-// 		expect(testCustomer.customerPhone).to.be.a("string");
-	// });
-// });
 
 describe("Pizza", function() {
 	it("creates a custom pizza", function() {
